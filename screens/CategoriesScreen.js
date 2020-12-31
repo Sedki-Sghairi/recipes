134ca709bfc1f2a7f6e94091624f511dd32174cb
Sedki-Sghairi <sedkisghairi3@gmail.com>
2020-12-31 11:39:36 +0400

    display dummy data in a grid

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { CATEGORIES } from '../data/dummy-data';
 
-export default function CategoriesScreen(props) {
-	console.log(props);
+const renderGridItem = (itemData) => {
 	return (
-		<View>
-			<Text style={styles.bold}>hello from CategoriesScreen</Text>
-			<Button title="next" onPress={() => props.navigation.navigate('CategoryMeals')} />
+		<View style={styles.gridItem}>
+			<Text style={styles.bold}>{itemData.item.title}</Text>
 		</View>
 	);
+};
+
+export default function CategoriesScreen(props) {
+	return <FlatList data={CATEGORIES} numColumns={2} renderItem={renderGridItem} />;
 }
 
 const styles = StyleSheet.create({
 	bold: {
 		fontWeight: 'bold'
+	},
+	gridItem: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		margin: 15,
+		height: 100
 	}
 });
