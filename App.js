45d922c317bd53480dc1236bd842cb799ec24d99
Sedki-Sghairi <sedkisghairi3@gmail.com>
2020-12-31 08:44:16 +0400

    AppContainr setup

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import AppContainer, { MealsNavigator } from './navigation/MealNavigator';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		// <View style={styles.container}>
+		//   <Text>Open up App.js to start working on your app!</Text>
+		//   <StatusBar style="auto" />
+		// </View>
+		<AppContainer />
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function MealDetailScreen() {
+	return (
+		<View>
+			<Text>MealDetailScreen</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({});
