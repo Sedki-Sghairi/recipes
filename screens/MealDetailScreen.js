e8997b17509d8271bf51ff2f37f8f0a881a66842
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-01-03 19:07:56 +0400

    adding MealDetailsScreen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 import { MEALS } from '../data/dummy-data';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import MyHeaderButton from '../components/HeaderButton';
+import colors from '../constants/colors';
 export default function MealDetailScreen(props) {
 	const mealId = props.navigation.getParam('mealId');
 	const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 	return (
-		<View>
-			<Text>ok</Text>
-		</View>
+		<ScrollView>
+			<Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+			<View style={styles.details}>
+				<Text>{selectedMeal.duration}m</Text>
+				<Text>{selectedMeal.complexity.toUpperCase()}</Text>
+				<Text>{selectedMeal.affordability.toUpperCase()}</Text>
+			</View>
+			<Text style={styles.title}>Ingredients</Text>
+			{selectedMeal.ingredients.map((item) => {
+				return (
+					<Text style={styles.txt} key={item}>
+						{item}
+					</Text>
+				);
+			})}
+			<Text style={styles.title}>Steps</Text>
+			{selectedMeal.steps.map((item) => {
+				return (
+					<Text style={styles.txt} key={item}>
+						{item}
+					</Text>
+				);
+			})}
+		</ScrollView>
 	);
 }
 MealDetailScreen.navigationOptions = (data) => {
@@ -25,4 +47,29 @@ MealDetailScreen.navigationOptions = (data) => {
 		)
 	};
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	image: {
+		width: '100%',
+		height: 200
+	},
+	details: {
+		flexDirection: 'row',
+		padding: 15,
+		justifyContent: 'space-around'
+	},
+	title: {
+		textAlign: 'center',
+		fontWeight: 'bold',
+		color: colors.winter,
+		fontSize: 17,
+		marginVertical: 10,
+		letterSpacing: 1.5
+	},
+	txt: {
+		textAlign: 'left',
+		marginHorizontal: 10,
+		padding: 5,
+		borderBottomColor: colors.earth,
+		borderBottomWidth: 1
+	}
+});
